8ef639604957d525892d26b0dcf36c1e8a08c2b1
aeon <yasunofabio@outlook.com.br>
2023-07-18 22:28:31 -0400

    feat: adiciona entidade conta

---
+++ src/main/java/br/com/banco/entity/Conta.java
@@ -0,0 +1,23 @@
+package br.com.banco.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity(name = "conta")
+public class Conta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nomeResponsavel", length = 50, nullable = false)
+	private String nomeResponsavel;
+	
+}
