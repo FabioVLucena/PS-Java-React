497409752971ab87fbb2b47d14bc576444b4124c
aeon <yasunofabio@outlook.com.br>
2023-07-18 23:16:50 -0400

    feat: adiciona anotacao repository ao transferenciaRepository

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import br.com.banco.entity.Transferencia;
 
+@Repository
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long>{
 	
 	@Query("from transferencia where conta_id = :contaId")
