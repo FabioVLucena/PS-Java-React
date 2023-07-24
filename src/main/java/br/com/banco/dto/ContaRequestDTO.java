323f7dedd0b4bf9ad8706e22f9260143436e0457
aeon <yasunofabio@outlook.com.br>
2023-07-23 21:56:01 -0400

    feat: adiciona ContaRequestDTO e dependencia spring validation ao pom

---
+++ src/main/java/br/com/banco/dto/ContaRequestDTO.java
@@ -0,0 +1,14 @@
+package br.com.banco.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class ContaRequestDTO {
+
+	@NotNull(message = "Numero é um campo obrigatório.")
+	@NotEmpty(message = "Numero não pode ser vazio.")
+	@NotBlank(message = "Numero não pode ser vazio.")
+	private Long numero;
+	
+}
