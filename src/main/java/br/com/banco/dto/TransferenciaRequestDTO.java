661e0d6eae3eb5530fc4cb4ea2d1e949cb2f83b6
aeon <yasunofabio@outlook.com.br>
2023-07-23 22:01:19 -0400

    feat: altera nome do ContaRequestDTO para TransferenciaRequestDTO

--- src/main/java/br/com/banco/dto/TransferenciaRequestDTO.java
+++ src/main/java/br/com/banco/dto/TransferenciaRequestDTO.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-public class ContaRequestDTO {
+public class TransferenciaRequestDTO {
 
 	@NotNull(message = "Numero é um campo obrigatório.")
 	@NotEmpty(message = "Numero não pode ser vazio.")
