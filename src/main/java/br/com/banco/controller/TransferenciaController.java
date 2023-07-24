583b3f880dc29c4b6d7d24f6e2b66f27309e71b2
aeon <yasunofabio@outlook.com.br>
2023-07-23 22:21:25 -0400

    feat: adiciona TransferenciaController e endpoint de filtro

---
+++ src/main/java/br/com/banco/controller/TransferenciaController.java
@@ -0,0 +1,35 @@
+package br.com.banco.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.banco.dto.TransferenciaRequestDTO;
+import br.com.banco.entity.Transferencia;
+import br.com.banco.exception.WarningException;
+import br.com.banco.service.TransferenciaService;
+
+@RestController
+@RequestMapping("/transferencias")
+public class TransferenciaController {
+
+	private TransferenciaService transferenciaService;
+	
+	public TransferenciaController(TransferenciaService transferenciaService) {
+		this.transferenciaService = transferenciaService;
+	}
+	
+	@PostMapping
+	public List<Transferencia> findTransferencia(@RequestBody TransferenciaRequestDTO filtro) throws WarningException {
+		
+		Long contaNumero = filtro.getContaNumero();
+		
+		List<Transferencia> transferenciaList = this.transferenciaService.findTransferencia(contaNumero, null, null, null);
+		
+		return transferenciaList;
+	}
+	
+}
